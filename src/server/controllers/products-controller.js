33a5260542f7f8ff3161d98718a203b7e53b139c
Bogdan Mazur <49879328+bmazurme@users.noreply.github.com>
2023-07-22 21:58:44 +0300

    fix: components

--- src/server/controllers/products-controller.js
+++ src/server/controllers/products-controller.js
@@ -4,9 +4,9 @@ const fileName = 'src/mocks/products.csv';
 
 const getProducts = async (req, res, next) => {
   try {
-    const products = await parseCSV(fileName);
+    const data = await parseCSV(fileName);
 
-    res.send(products);
+    res.send(data);
   } catch (err) {
     next(err);
   }
